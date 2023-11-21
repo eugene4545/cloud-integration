6868ff1ce394328af7aa517015cf30af08866f86
eugene4545 <eugeneonuoha72@gmail.com>
2023-11-21 13:29:46 +0100

    google maps

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MapContainer from './components/MapContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Map</h1>
+      <MapContainer />
+    </div>
+  );
+}
+
+export default App;
